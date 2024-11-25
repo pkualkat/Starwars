9a0b533583d0f94df43e87363a7f8552d1a18b33
pkualkat <pkualkat@ivytech.edu>
2024-11-24 22:26:21 -0500

    strar  wars app

---
+++ Films.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+export default function Films() {
+  return (
+    <View style={styles.container}>
+      <Text>Star Wars Films</Text>
+    </View>
+  );
+}
